8131eec67e7812eef8bd4fe49da999534b27817a
Lokesh-128 <padamatilokesh123@gmail.com>
2025-08-02 10:09:11 +0530

    Add files via upload

---
+++ OOPSKILLTASK1/src/Mypackage/sum.java
@@ -0,0 +1,13 @@
+package Mypackage;
+
+public class sum {
+	
+		public static void main(String[] args) {
+			int a=10;
+			int b=20;
+			System.out.println("Result of Sum: " +(a+b));
+
+		}
+
+	}
+
---
+++ OOPSKILLTASK1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module OOPSKILLTASK1 {
+}
